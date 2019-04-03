dd84fce128acb179f1d50c0d83947e974ec688fe
CodeMongerX <37663008+CodeMongerX@users.noreply.github.com>
2019-04-03 22:14:08 +0530

    2 types of structs
    examples for declaring and writing structs

--- structs/main.go
+++ structs/main.go
@@ -5,17 +5,40 @@ import "fmt"
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
+}
+
+type contactInfo struct {
+	phoneNo int
+	email   string
 }
 
 func main() {
 
-	//alex := person{firstName: "Rajeev", lastName: "Shivaram"}
+	//alex := person{firstName: "alex", lastName: "Anderson"}
 	//fmt.Println(alex)
 
-	var rajeev person
+	// var alex person
 
-	rajeev.firstName = "Rajeev"
-	rajeev.lastName = "Shivaram"
+	// alex.firstName = "alex"
+	// alex.lastName = "Anderson"
+	// fmt.Printf("%+v\n", alex)
 
-	fmt.Printf("%+v\n", rajeev)
+	var alex person
+
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson"
+	alex.contact.email = "alex.anderson@gmail.com"
+	alex.contact.phoneNo = 9876543210
+
+	alex2 := person{firstName: "alex",
+		lastName: "Anderson",
+		contact: contactInfo{
+			email:   "alex.anderson2@gmail.com",
+			phoneNo: 9876543210,
+		},
+	}
+
+	fmt.Printf("%+v", alex)
+	fmt.Printf("%+v", alex2)
 }

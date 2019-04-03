ec6ec9cfd86f67327d5e8b689fb206a86a31f49b
CodeMongerX <37663008+CodeMongerX@users.noreply.github.com>
2019-04-03 21:59:56 +0530

    struct declarations
    two types of struct declarations using normal and var type

--- structs/main.go
+++ structs/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type person struct {
 	firstName string
 	lastName  string
@@ -7,4 +9,13 @@ type person struct {
 
 func main() {
 
+	//alex := person{firstName: "Rajeev", lastName: "Shivaram"}
+	//fmt.Println(alex)
+
+	var rajeev person
+
+	rajeev.firstName = "Rajeev"
+	rajeev.lastName = "Shivaram"
+
+	fmt.Printf("%+v\n", rajeev)
 }

61309b7af89e10aea19afc72be59fb8384a7b5d6
CodeMongerX <37663008+CodeMongerX@users.noreply.github.com>
2019-04-03 21:25:01 +0530

    added some testing funcs

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -16,4 +19,19 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "Four of Spades" {
 		t.Errorf("Expected Four of Spades but got %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToDeckAndNewTestDeckFromFile(t *testing.T) {
+	os.Remove("_deckTestFile")
+
+	d := newDeck()
+	d.saveToFile("_deckTestFile")
+
+	loadFile := readFromFile("_deckTestFile")
+
+	if len(loadFile) != len(d) {
+		t.Errorf("File not matching!")
+	}
+
+	os.RemoveAll("_deckTestFile")
 }

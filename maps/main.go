f80a6a4f483184f07ab29c30840cc561e9960a52
CodeMongerX <37663008+CodeMongerX@users.noreply.github.com>
2019-04-05 23:24:53 +0530

    added print function

--- maps/main.go
+++ maps/main.go
@@ -9,5 +9,12 @@ func main() {
 		"Green": "#345",
 	}
 
-	fmt.Printf("%+v", colors)
+	//fmt.Printf("%+v", colors)
+	print(colors)
+}
+
+func print(m map[string]string) {
+	for ckey, cval := range m {
+		fmt.Println("key = ", ckey, "& values = ", cval, ";\n")
+	}
 }
